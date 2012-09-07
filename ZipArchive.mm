d03aa1ef055564788eace19f1249f29efa69c576
Clark Williams <clarkw@s-i-g-h.com>
2012-09-07 11:48:13 -0700

    Removed warnings because of base-type resolution. Added parens to eliminate warnings on assigns within bool-expression of if statements.

--- ZipArchive.mm
+++ ZipArchive.mm
@@ -166,7 +166,7 @@
 		unz_global_info  globalInfo = {0};
 		if( unzGetGlobalInfo(_unzFile, &globalInfo )==UNZ_OK )
 		{
-			NSLog(@"%d entries in the zip file",globalInfo.number_entry );
+			NSLog(@"%ld entries in the zip file",globalInfo.number_entry );
 		}
 	}
 	return _unzFile!=NULL;
